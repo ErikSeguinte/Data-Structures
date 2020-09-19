a1d036413ab8bca5de478d9888c7bd11ad822d83
Erik <spamfactorx01@gmail.com>
2020-09-18 22:02:17 -0700

    BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,5 +1,5 @@
 """
-Binary search trees are a data structure that enforce an ordering over 
+Binary search trees are a data structure that enforce an ordering over
 the data they store. That ordering in turn makes it a lot more efficient 
 at searching for a particular piece of data in the tree. 
 
@@ -9,6 +9,7 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+from collections import deque
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,48 +18,121 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BSTNode(value)
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        else:
+            if target < self.value:
+                if self.left:
+                    return self.left.contains(target)
+            else:
+                if self.right:
+                    return self.right.contains(target)
+        return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        if self.left:
+            self.left.for_each(fn)
+        fn(self.value)
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self):
-        pass
-
+        if self.left:
+            self.left.in_order_print()
+        print(self.value)
+        if self.right:
+            self.right.in_order_print()
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self):
-        pass
+        print(self.value)
+        q = deque()
+        if self.left:
+            q.append(self.left)
+        if self.right:
+            q.append(self.right)
+
+        while q:
+            node = q.popleft()
+            print(node.value)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self):
-        pass
+        q = list()
+
+        if self.left:
+            q.append(self.left)
+        if self.right:
+            q.append(self.right)
+
+        print(self.value)
+
+        while q:
+            node = q.pop()
+            print(node.value)
+            if node.right:
+                q.append(node.right)
+            if node.left:
+                q.append(node.left)
+
 
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self):
-        pass
+        print(self.value)
+        if self.left:
+            self.left.pre_order_dft()
+        if self.right:
+            self.right.pre_order_dft()
 
     # Print Post-order recursive DFT
     def post_order_dft(self):
-        pass
+        if self.left:
+            self.left.post_order_dft()
+        if self.right:
+            self.right.post_order_dft()
+        print(self.value)
+
+    def in_order_dft(self):
+        if self.left:
+            self.left.in_order_dft()
+        print(self.value)
+        if self.right:
+            self.right.in_order_dft()
 
 """
 This code is necessary for testing the `print` methods
