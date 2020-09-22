13ced4ca28762cdada5a9aa276e3a21b15f64fdc
Erik <spamfactorx01@gmail.com>
2020-09-21 19:03:23 -0700

    passes insert

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,9 +1,18 @@
 class Heap:
+    """ Uses a 1 based array to make index math more intuitive."""
     def __init__(self):
-        self.storage = []
+        self._storage = [None]
+        self.size = 0
+
+    @property
+    def storage(self):
+        # skips the 0th element to make sure tests work.
+        return self._storage[1:]
 
     def insert(self, value):
-        pass
+        self._storage.append(value)
+        self.size += 1
+        self._bubble_up(self.size)
 
     def delete(self):
         pass
@@ -12,10 +21,17 @@ class Heap:
         pass
 
     def get_size(self):
-        pass
+        return self.size
 
     def _bubble_up(self, index):
-        pass
+        while index // 2 > 0:
+            if self._storage[index] > self._storage[index // 2]:
+                self._storage[index], self._storage[index // 2] = (
+                    self._storage[index  // 2],
+                    self._storage[index],
+                )
+
+            index = index // 2
 
     def _sift_down(self, index):
         pass
